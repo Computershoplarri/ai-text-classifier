915259e29e6ff862d5930655c4c16a6f8ac48d86
Computershoplarri <computershop768@gmail.com>
2026-04-20 22:46:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
+import os
+import pickle
+
+base_dir = os.path.dirname(__file__)
+
+model = pickle.load(open(os.path.join(base_dir, "model", "model.pkl"), "rb"))
+vectorizer = pickle.load(open(os.path.join(base_dir, "model", "vectorizer.pkl"), "rb"))
 
 
