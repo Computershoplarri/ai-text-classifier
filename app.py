b44f606ebaf1792b05590c37146d02a9c7a3d4c5
Computershoplarri <computershop768@gmail.com>
2026-04-22 19:07:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
 import os
 import pickle
+import streamlit as st
 
-base_dir = os.path.dirname(__file__)
-
+# load model
 model = pickle.load(open("model.pkl", "rb"))
 vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+# labels
+labels = [
+    "alt.atheism",
+    "comp.graphics",
+    "comp.os.ms-windows.misc",
+    "comp.sys.ibm.pc.hardware",
+    "comp.sys.mac.hardware",
+    "comp.windows.x",
+    "misc.forsale",
+    "rec.autos",
+    "rec.motorcycles",
+    "rec.sport.baseball",
+    "rec.sport.hockey",
+    "sci.crypt",
+    "sci.electronics",
+    "sci.med",
+    "sci.space",
+    "soc.religion.christian",
+    "talk.politics.guns",
+    "talk.politics.mideast",
+    "talk.politics.misc",
+    "talk.religion.misc"
+]
+
+# UI
+st.title("🚀 AI Text Classifier")
+
+text = st.text_area("Enter text")
+
+if st.button("Predict"):
+    if text.strip() == "":
+        st.warning("Enter text first")
+    else:
+        vec = vectorizer.transform([text])
+        pred = model.predict(vec)[0]
+        st.success(f"Prediction: {labels[pred]}")
+
 
 
 
